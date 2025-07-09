8c9832745edf8397e360061d8f1fa1c1a2ec4298
prakash@treeshainfotech.com <prakash@treeshainfotech.com>
2025-07-09 16:56:17 +0530

    Fix customcert issue //namespace customcertelement_date\privacy;

--- mod/customcert/element/date/classes/element.php
+++ mod/customcert/element/date/classes/element.php
@@ -22,7 +22,7 @@
      * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
      */
 
-    namespace customcertelement_date;
+//    namespace customcertelement_date;
 
     defined('MOODLE_INTERNAL') || die();
 
--- mod/customcert/element/date/classes/privacy/provider.php
+++ mod/customcert/element/date/classes/privacy/provider.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace customcertelement_date\privacy;
+//namespace customcertelement_date\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
