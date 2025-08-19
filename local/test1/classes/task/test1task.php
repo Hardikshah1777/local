1f6a91d44ef3630b8281d67421535ec69b9d1fa2
prakash@treeshainfotech.com <prakash@treeshainfotech.com>
2025-08-19 09:53:28 +0530

    Change for run now button of cron

--- local/test1/classes/task/test1task.php
+++ local/test1/classes/task/test1task.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace local_test1\task;
 
 class test1task extends \core\task\scheduled_task
@@ -9,13 +8,17 @@ class test1task extends \core\task\scheduled_task
         return get_string('test1task', 'local_test1');
     }
 
+    public function can_run(): bool {
+        return true;
+        //return $this->is_component_enabled() || $this->get_run_if_component_disabled();
+    }
+
     public function execute() {
-        global $USER;
-        mtrace('cron working local/test1/classes/task/test1task.php');
-        email_to_user($USER, $USER,
-            get_string('test1task', 'local_test1'),
-            get_string('test1task', 'local_test1')
-        );
+        global $USER, $CFG;
+//        mtrace('cron working by cmd: php admin/tool/task/cli/schedule_task.php --execute="\local_test1\task\test1task"');
+        $mailtxt = get_string('test1task', 'local_test1');
+        $attechment = \html_writer::img($CFG->wwwroot.'/local/test1/pix/test1.jpg','Test1 image', ['width' => '50%']);
+        email_to_user($USER, $USER, $mailtxt, $mailtxt, $mailtxt, $attechment, 'test1.jpg');
         return true;
     }
 }
