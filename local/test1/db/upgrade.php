ac51ac9cf247abc624b6b1864ce2838858091481
prakash@treeshainfotech.com <prakash@treeshainfotech.com>
2025-09-17 09:53:40 +0530

    Change for check upgrade code work or not for courseuserinvoice table

--- local/test1/db/upgrade.php
+++ local/test1/db/upgrade.php
@@ -34,5 +34,30 @@ function xmldb_local_test1_upgrade($oldversion)
         upgrade_plugin_savepoint( true, 2023112700, 'local', 'test1' );
     }
 
+    if ($oldversion < 2023112700.01) {
+
+        // Define table courseuserinvoice to be created.
+        $table = new xmldb_table('courseuserinvoice');
+
+        // Adding fields to table courseuserinvoice.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('invoice', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table courseuserinvoice.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+
+        // Conditionally launch create table for courseuserinvoice.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Test1 savepoint reached.
+        upgrade_plugin_savepoint( true, 2023112700.01, 'local', 'test1' );
+        //upgrade_main_savepoint(true, 2022041903.00);
+    }
+
     return true;
 }
