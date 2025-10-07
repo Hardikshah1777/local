f81546f57d9a6404165a5e7c8e929b3550ebf0ab
prakash@treeshainfotech.com <prakash@treeshainfotech.com>
2025-10-07 11:10:14 +0530

    CURL example

--- test1.php
+++ test1.php
@@ -5,3 +5,29 @@
         <button type="submit" class="btn btn-primary" id="single_button65cc52ef65e1a2">Download</button>
     </form>
 </div>
+<?php
+
+// Initialize cURL session
+$ch = curl_init();
+
+// Set the URL
+curl_setopt($ch, CURLOPT_URL, "https://jsonplaceholder.typicode.com/users/2");
+
+// Return the transfer as a string
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Execute the request
+$response = curl_exec($ch);
+
+// Check for errors
+if(curl_errno($ch)){
+    echo 'Curl error: ' . curl_error($ch);
+}
+
+// Close the session
+curl_close($ch);
+
+// Display the response
+echo '<pre>';
+echo $response;
+echo '<pre>';
